0d80f8ba1421a038a439c50fa2409790b0420e73
wlfgang <14792753+wlfgang@users.noreply.github.com>
2024-08-09 06:58:18 -0400

    Wezterm leader key and assignments for panes

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,10 +1,17 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local mux = wezterm.mux
 local act = wezterm.action
 local config = {}
 
 -- shell
 config.default_prog = { "/usr/bin/zsh" }
+
+-- maximize on startup
+wezterm.on("gui-startup", function(cmd)
+    local _, _, window = mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
+end)
 
 -- window
 config.window_decorations = "RESIZE"
@@ -34,6 +41,9 @@ config.font_size = 14.0
 -- disable ligatures in most fonts
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
+-- leader key configuration
+config.leader = { key = "Space", mods = "ALT", timeout_milliseconds = 2000 }
+
 -- keys
 config.keys = {
     -- tabs
@@ -43,8 +53,22 @@ config.keys = {
     { key = "h", mods = "ALT", action = act.ActivateTabRelative(-1) },
     { key = "Enter", mods = "ALT", action = act.SpawnTab("CurrentPaneDomain") },
     { key = "q", mods = "ALT", action = act.CloseCurrentTab({ confirm = true }) },
-    -- panes
-    { key = "Space", mods = "ALT|CTRL|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+
+    -- split panes
+    { key = "|", mods = "SHIFT|LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+    -- move panes
+    { key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+    { key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+    { key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+    { key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+
+    -- resize panes
+    { key = "h", mods = "ALT|LEADER", action = act.AdjustPaneSize({ "Left", 5 }) },
+    { key = "l", mods = "ALT|LEADER", action = act.AdjustPaneSize({ "Right", 5 }) },
+    { key = "k", mods = "ALT|LEADER", action = act.AdjustPaneSize({ "Up", 5 }) },
+    { key = "j", mods = "ALT|LEADER", action = act.AdjustPaneSize({ "Down", 5 }) },
 }
 
 return config
